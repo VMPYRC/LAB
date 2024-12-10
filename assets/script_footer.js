fed3bbae8f2ed4f873c5b3cc9bc38b43da669bae
VMPYRC <90119648+VMPYRC@users.noreply.github.com>
2024-12-10 14:33:23 -0500

    renames

--- assets/script_footer.js
+++ assets/script_footer.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-	fetch('footerbar.html')
+	fetch('footer.html')
 			.then(response => response.text())
 			.then(data => {
-					document.getElementById('footerbar').innerHTML = data;
+				document.getElementById('footer-container').innerHTML = data;
 			})
 			.catch(error => console.error('Error loading footer:', error));
 };
--- assets/script_headerbar.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-	fetch('headerbar.html')  // Fetch the header.html file
-			.then(response => response.text())  // Parse the file as text
-			.then(data => {
-					document.getElementById('headerbar').innerHTML = data;  // Inject the content into the headerbar div
-			})
-			.catch(error => console.error('Error loading header:', error));  // Handle any errors
-};
